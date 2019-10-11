d3cd0e300fbbe29cdafd0fc454b9801775f44867
yanghao <1091174771@qq.com>
2019-10-11 16:35:54 +0800

    首次提交

---
+++ .postcssrc.js
@@ -0,0 +1,31 @@
+// https://github.com/michael-ciniawsky/postcss-load-config
+
+module.exports = {
+  plugins: {
+    "postcss-import": {},
+    "postcss-url": {},
+    // to edit target browsers: use "browserslist" field in package.json
+    // "autoprefixer": {},
+    "postcss-aspect-ratio-mini": {},
+    "postcss-write-svg": {
+      utf8: false
+    },
+    "postcss-preset-env": {},
+    "postcss-px-to-viewport": {
+      unitToConvert: "px", //(String) 需要转换的单位，默认为"px"
+      viewportWidth: 750, // (Number) 视窗的宽度，对应的是我们设计稿的宽度，一般是750
+      viewportHeight: 1334, // (Number) 视窗的高度，根据750设备的宽度来指定，一般指定1334，也可以不配置
+      unitPrecision: 3, // (Number) 指定`px`转换为视窗单位值的小数位数（很多时候无法整除）
+      viewportUnit: "vw", // (String) 指定需要转换成的视窗单位，建议使用vw
+      // fontViewportUnit: "vw", //(String) 字体使用的视口单位
+      selectorBlackList: [".ignore", ".hairlines"], // (Array) 指定不转换为视窗单位的类，可以自定义，可以无限添加,建议定义一至两个通用的类名
+      minPixelValue: 1, // (Number) 小于或等于`1px`不转换为视窗单位，你也可以设置为你想要的值
+      mediaQuery: false // (Boolean) 允许在媒体查询中转换`px`
+    },
+    cssnano: {
+      preset: "advanced",
+      autoprefixer: false,
+      "postcss-zindex": false
+    }
+  }
+};
---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div class="hello-box">
+      <div class="hello-box-item" v-for="(i,index) in tabList" :key="index">
+        <!-- <img class="box-item-img" src="./assets/image/c2.png" alt=""> -->
+        <img class="box-item-img" :src="i.img" alt="">
+        <p style="font-size: 14px;">{{i.name}}</p>
+      </div>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      tabList: [
+        {name: "测试1", img: "../static/image/c1.png"},
+        {name: "测试2", img: "../static/image/c2.png"},
+        {name: "测试3", img: "../static/image/c3.png"},
+        {name: "测试4", img: "../static/image/c4.png"}
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+body,p{
+  margin: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  box-sizing: border-box;
+}
+.hello-box{
+  /* width: calc(100vw - 100px); */
+  /* height: 13vh; */
+  display: flex;
+  padding: 10px;
+}
+.hello-box-item{
+  height: 100%;
+  width: calc(100vw - 200px);
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.box-item-img{
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+  opacity: .7;
+  margin-bottom: 14px;
+  box-shadow: 4px 4px 20px rgba(250, 67, 106, 0.3);
+}
+</style>
